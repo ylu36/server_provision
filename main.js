b0861e9a6b416634f11d6b29d35905ec87e0a890
ylu36 <ylu36@ncsu.edu>
2018-02-08 18:56:50 -0800

    finished aws provisioning

--- main.js
+++ main.js
@@ -20,6 +20,16 @@ var client =
 	{
 		needle.get("https://api.digitalocean.com/v2/regions", {headers:headers}, onResponse)
 	},
+	
+	listImages: function( onResponse )
+	{
+		needle.get("https://api.digitalocean.com/v2/images", {headers:headers}, onResponse)
+	},
+
+    getDroplet: function (id, onResponse) 
+    {
+    	needle.get("https://api.digitalocean.com/v2/droplets/"+id, {headers:headers}, onResponse)
+    },
 
 	createDroplet: function (dropletName, region, imageName, onResponse)
 	{
@@ -38,9 +48,14 @@ var client =
 			"private_networking":null
 		};
 
-		console.log("Attempting to create: "+ JSON.stringify(data) );
+	//	console.log("Attempting to create: "+ JSON.stringify(data) );
 
 		needle.post("https://api.digitalocean.com/v2/droplets", data, {headers:headers,json:true}, onResponse );
+	},
+
+	destroyDropplet: function (id, onResponse) {
+
+		needle.delete('https://api.digitalocean.com/documentation/v2/'+id, null,  {headers:headers,json:true}, onResponse );
 	}
 };
 
@@ -52,7 +67,7 @@ var client =
 client.listRegions(function(error, response)
 {
 	var data = response.body;
-	console.log( JSON.stringify(response.body) );
+//	console.log( JSON.stringify(response.body) );
 
 	if( response.headers )
 	{
@@ -63,7 +78,8 @@ client.listRegions(function(error, response)
 	{
 		for(var i=0; i<data.regions.length; i++)
 		{
-	
+			var dc = data.regions[i];
+		//	console.log(dc.slug);
 		}
 	}
 });
@@ -76,22 +92,47 @@ client.listRegions(function(error, response)
 // - Print out a list of available system images, that are AVAILABLE in a specified region.
 // - use 'slug' property
 
+
+// // #############################################
+// // #1 Print out a list of available regions
+// // Comment out when completed.
+// // https://developers.digitalocean.com/documentation/v2/#list-all-regions
+// // use 'slug' property
+// client.listImages(function(error, response)
+// {
+// 	var data = response.body;
+// 	//console.log( JSON.stringify(response.body) );
+
+// 	if( response.headers )
+// 	{
+// 		console.log( "Calls remaining", response.headers["ratelimit-remaining"] );
+// 	}
+
+// 	if( data.images )
+// 	{
+// 		for(var i=0; i<data.images.length; i++)
+// 		{
+// 			var dc = data.images[i];//console.log(dc.slug);
+// 		}
+// 	}
+// });
 // #############################################
 // #3 Create an droplet with the specified name, region, and image
 // Comment out when completed. ONLY RUN ONCE!!!!!
-// Write down/copy droplet id.
-// var name = "UnityId"+os.hostname();
-// var region = ""; // Fill one in from #1
-// var image = ""; // Fill one in from #2
-// client.createDroplet(name, region, image, function(err, resp, body)
-// {
-// 	console.log(body);
-// 	// StatusCode 202 - Means server accepted request.
-// 	if(!err && resp.statusCode == 202)
-// 	{
-// 		console.log( JSON.stringify( body, null, 3 ) );
-// 	}
-// });
+// // Write down/copy droplet id.
+  var name = "UnityId"+os.hostname();
+  var region = "nyc3"; // Fill one in from #1
+ var image = "ubuntu-16-04-x64"; // Fill one in from #2
+
+ // client.createDroplet(name, region, image, function(err, resp, body)
+ // {
+ // 	console.log(body);
+ // 	// StatusCode 202 - Means server accepted request.
+ // 	if(!err && resp.statusCode == 202)
+	// {
+ // 		console.log( JSON.stringify( body, null, 3 ) );
+ // 	}
+ // });
 
 // #############################################
 // #4 Extend the client to retrieve information about a specified droplet.
@@ -99,8 +140,12 @@ client.listRegions(function(error, response)
 // https://developers.digitalocean.com/documentation/v2/#retrieve-an-existing-droplet-by-id
 // REMEMBER POST != GET
 // Most importantly, print out IP address!
-var dropletId = "3788359";
-
+var dropletId = "80973330";
+client.getDroplet(dropletId, function(err, response, body) {
+	var data = response.body;
+	// console.log(data);
+	console.log(body.droplet.networks.v4[0].ip_address);
+});
 // #############################################
 // #5 In the command line, ping your server, make sure it is alive!
 // ping xx.xx.xx.xx
@@ -113,11 +158,8 @@ var dropletId = "3788359";
 // HINT #2, needle.delete(url, data, options, callback), data needs passed as null.
 // No response body will be sent back, but the response code will indicate success.
 // Specifically, the response code will be a 204, which means that the action was successful with no returned body data.
-// 	if(!err && resp.statusCode == 204)
-// 	{
-//			console.log("Deleted!");
-// 	}
-
+client.destroyDropplet(dropletId);
+// 	
 // #############################################
 // #7 In the command line, ping your server, make sure it is dead!
 // ping xx.xx.xx.xx
---
+++ main1.js
@@ -0,0 +1,61 @@
+require('dotenv').load();
+
+// Load the SDK for JavaScript
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'us-east-2',
+  accessKeyId: process.env.accessKeyId, 
+  secretAccessKey: process.env.secretAccessKey});
+// Create EC2 service object
+var ec2 = new AWS.EC2();
+
+var params = {
+   ImageId: 'ami-965e6bf3', // amzn-ami-2011.09.1.x86_64-ebs
+   InstanceType: 't2.micro',
+   MinCount: 1,
+   MaxCount: 1
+};
+
+//printStatuses();
+//Create the instance
+ec2.runInstances(params, function(err, data) {
+   if (err) {
+      console.log("Could not create instance", err);
+      return;
+   }
+   var instanceId = data.Instances[0].InstanceId;
+   console.log("Created instance", instanceId);
+   var paramsForDI = 
+		{
+			InstanceIds: [instanceId]
+		};
+   // Add tags to the instance
+   params = {Resources: [instanceId], Tags: [
+      {
+         Key: 'hw1b',
+         Value: 'hw 1b'
+      }
+   ]};
+   ec2.createTags(params, function(err) {
+      console.log("Tagging instance", err ? "failure" : "success");
+   });
+
+
+setTimeout(function(){ 
+   ec2.describeInstances(paramsForDI, function(error, data)
+		{
+			if(error)
+			{
+				console.log("AWS Describe Instances Error - " + error + "\n");
+			}
+			else
+			{
+				var awsPubDNS = data.Reservations[0].Instances[0].PublicDnsName;
+				var awsPubIP  = data.Reservations[0].Instances[0].PublicIpAddress;
+				console.log("AWS Instance Public DNS Name - " + awsPubDNS + "\n");
+				console.log("AWS Instance Public IP Address - " + awsPubIP + "\n");
+				
+			}
+		});
+});
+}, 10000);
